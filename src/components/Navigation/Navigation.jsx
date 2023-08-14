7d8e0f83a20428c02a8c1328562c274c7456f735
Ihor Zuikov <ihor.zuikov@gmail.com>
2023-08-14 14:19:20 +0300

    first react-app

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,12 @@
+import Logo from '../Logo/Logo';
+import './Header.scss';
+
+function Header() {
+    return (
+        <header className="header">
+            <Logo />
+        </header>
+    );
+}
+
+export default Header;
